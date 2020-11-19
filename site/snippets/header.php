b79fb9242ad243068555f336c03da94b0c4f8289
omarmhmmd <omar.mhmmd@gmail.com>
2020-11-18 20:48:51 -0800

    safe

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -2,7 +2,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <link rel="icon" href="/assets/favicon.png" >
-<title>CiNEOLA ------ <?= $page->title() ?></title>
+<title>CiNEOLA | <?= $page->title() ?></title>
 <?= css('assets/css/reset.css') ?>
 <?= css('assets/css/index.css') ?>
 <script src="https://cdn.jsdelivr.net/npm/vue"></script>
